26649732f2d5487386ea887cb288119e0772723d
Smitha <smitha@eregowda.com>
2026-07-25 14:00:49 +0530

    app updated

--- backend/authservice/src/app.js
+++ backend/authservice/src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/',(req,res)=>{
     res.status(200).json({
-        staus:"Auth Service Check!!!"
+        staus:"Auth Service Check!!"
     });
 });
 
