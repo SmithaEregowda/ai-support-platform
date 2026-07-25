eabb3935bc2d156a46424cc963cfba01b6d9d075
SmithaEregowda <smithaeregowda@gmail.com>
2026-07-25 14:02:41 +0530

    health checkup added

--- backend/authservice/src/app.js
+++ backend/authservice/src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 8080;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/',(req,res)=>{
+app.use('/health',(req,res)=>{
     res.status(200).json({
         staus:"Auth Service Check!!"
     });
