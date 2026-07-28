249d8c06bb6a61918b2efe002e478bb24b43eb83
SmithaEregowda <smithaeregowda@gmail.com>
2026-07-28 20:19:23 +0530

    ticketservice setup

---
+++ backend/ticketservice/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/health',(req,res)=>{
+    res.status(200).json({
+        staus:"Ticket Service Check!!"
+    });
+});
+
+export default app;
